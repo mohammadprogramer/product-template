39cba20d44ea17af552e00096d53b516466055da
Mohammad Zolfalizadeh <mohammadprogramer63@gmail.com>
2025-03-12 19:48:57 +0330

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,5 +1,24 @@
 <?php
 
+
+
+function my_theme_enqueue_scripts() {
+    wp_enqueue_script('custom-script', get_template_directory_uri() . '/js/script.js', array(), null, true);
+}
+add_action('wp_enqueue_scripts', 'my_theme_enqueue_scripts');
+
+function my_theme_enqueue_styles() {
+    wp_enqueue_style('my-theme-style', get_stylesheet_uri());
+}
+add_action('wp_enqueue_scripts', 'my_theme_enqueue_styles');
+
+function my_theme_register_menus() {
+    register_nav_menus(array(
+        'primary' => __('منوی اصلی', 'my-theme'),
+    ));
+}
+add_action('init', 'my_theme_register_menus');
+
 function my_theme_add_woocommerce_support() {
     add_theme_support('woocommerce');
 }
@@ -11,17 +30,6 @@ add_action('wp_enqueue_scripts', 'my_theme_enqueue_woocommerce_styles');
 
 
 
-
-function custom_product_page() {
-    if (is_product()) {
-        // اضافه کردن استایل‌های سفارشی
-        wp_enqueue_style('custom-product-style', get_template_directory_uri() . '/css/custom-product.css');
-
-        // اضافه کردن اسکریپت‌های سفارشی
-        wp_enqueue_script('custom-product-script', get_template_directory_uri() . '/js/custom-product.js', array('jquery'), null, true);
-    }
-}
-add_action('wp_enqueue_scripts', 'custom_product_page');
 
 function custom_product_price($price, $product) {
     return '<span class="custom-price">' . $price . '</span>';
