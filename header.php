c3c56a4b99f0e8a7c090f71b0773e5ee0784411b
Mohammad Zolfalizadeh <mohammadprogramer63@gmail.com>
2025-03-12 20:08:51 +0330

    Update header.php

--- header.php
+++ header.php
@@ -1,3 +1,8 @@
+<?php
+/**
+ * The header for our theme
+ */
+?>
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
@@ -6,38 +11,7 @@
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
-    <header class="site-header">
-        <div class="container">
-            <!-- لوگو -->
-            <div class="site-logo">
-                <?php
-                if (has_custom_logo()) {
-                    the_custom_logo();
-                } else {
-                    ?>
-                    <a href="<?php echo esc_url(home_url('/')); ?>">
-                        <h1><?php bloginfo('name'); ?></h1>
-                    </a>
-                    <?php
-                }
-                ?>
-            </div>
-
-            <!-- منو -->
-            <nav class="site-navigation">
-                <!-- دکمه همبرگر برای موبایل -->
-                <button class="hamburger-menu" aria-label="منو">
-                    <span class="hamburger-icon"></span>
-                </button>
-
-                <!-- منوی اصلی -->
-                <?php
-                wp_nav_menu(array(
-                    'theme_location' => 'primary',
-                    'menu_class'     => 'primary-menu',
-                    'container'     => false,
-                ));
-                ?>
-            </nav>
-        </div>
-    </header>
+    <?php
+    // فراخوانی تمپلیت هدر سفارشی
+    get_template_part('header-custom');
+    ?>
