3919a4a964e1ef9387a96a81658dc73f14ed525e
Mohammad Zolfalizadeh <mohammadprogramer63@gmail.com>
2025-03-14 18:49:21 +0330

    Update single.php

--- single.php
+++ single.php
@@ -2,13 +2,6 @@
 get_header(); // فراخوانی هدر
 ?>
 
-<section class="custom-section">
-    <div class="container">
-        <h2>این یک بخش سفارشی است</h2>
-        <p>این محتوا بین هدر و فوتر نمایش داده می‌شود.</p>
-    </div>
-</section>
-
 <main id="primary" class="site-main">
     <?php
     while (have_posts()) :
