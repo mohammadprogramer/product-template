cab781d2044ff0fa9ae9e55b261e2baa8bbaa57b
Mohammad Zolfalizadeh <mohammadprogramer63@gmail.com>
2025-03-14 11:40:31 +0330

    Update footer.php

---
+++ footer.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ */
+?>
+
+</div><!-- #content -->
+
+<?php
+// فراخوانی فوتر سفارشی
+get_template_part('footer-custom');
