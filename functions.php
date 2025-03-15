7fdc3b2743ba9a2c62666d1ad639f845f73ab066
Mohammad Zolfalizadeh <mohammadprogramer63@gmail.com>
2025-03-15 20:00:42 +0330

    Update functions.php

--- functions.php
+++ functions.php
@@ -2,13 +2,10 @@
 
 require_once get_template_directory() . '/class-wp-bootstrap-navwalker.php';
 
-
-
 function my_theme_enqueue_custom_script() {
     wp_enqueue_script('custom-script', get_template_directory_uri() . '/js/script.js', array('jquery'), null, true);
 }
 add_action('wp_enqueue_scripts', 'my_theme_enqueue_custom_script');
-
 
 function my_theme_enqueue_styles() {
     // فراخوانی فایل CSS اصلی تم
@@ -16,23 +13,38 @@ function my_theme_enqueue_styles() {
 
     // فراخوانی فایل‌های CSS اضافی (اگر دارید)
     wp_enqueue_style('custom-style', get_template_directory_uri() . '/css/custom.css');
+
+    // افزودن فونت Poppins
+    wp_enqueue_style(
+        'google-fonts-poppins',
+        'https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;600;800&display=swap',
+        array(),
+        null
+    );
+
+    // افزودن فونت Open Sans
+    wp_enqueue_style(
+        'google-fonts-open-sans',
+        'https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap',
+        array(),
+        null
+    );
+
+    // افزودن فونت آیکون‌های Font Awesome
+    wp_enqueue_style(
+        'font-awesome',
+        'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css',
+        array(),
+        '5.15.4'
+    );
 }
 add_action('wp_enqueue_scripts', 'my_theme_enqueue_styles');
-
-
-
-function my_theme_enqueue_fonts() {
-    wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css2?family=Poppins:wght@800&family=Open+Sans:wght@400;700&display=swap');
-}
-add_action('wp_enqueue_scripts', 'my_theme_enqueue_fonts');
-
 
 function my_theme_support_elementor() {
     // پشتیبانی از المنتور
     add_theme_support('elementor');
 }
 add_action('after_setup_theme', 'my_theme_support_elementor');
-
 
 function my_theme_enqueue_scripts() {
     // فراخوانی فایل CSS بوت‌استرپ
@@ -49,9 +61,6 @@ function my_theme_enqueue_scripts() {
 }
 add_action('wp_enqueue_scripts', 'my_theme_enqueue_scripts');
 
-
-
-
 function my_theme_register_menus() {
     register_nav_menus(array(
         'primary' => __('منوی اصلی', 'my-theme'),
@@ -59,20 +68,15 @@ function my_theme_register_menus() {
 }
 add_action('init', 'my_theme_register_menus');
 
-
-
-
 function my_theme_add_woocommerce_support() {
     add_theme_support('woocommerce');
 }
 add_action('after_setup_theme', 'my_theme_add_woocommerce_support');
+
 function my_theme_enqueue_woocommerce_styles() {
     wp_enqueue_style('woocommerce-style', get_template_directory_uri() . '/woocommerce.css');
 }
 add_action('wp_enqueue_scripts', 'my_theme_enqueue_woocommerce_styles');
-
-
-
 
 function custom_product_price($price, $product) {
     return '<span class="custom-price">' . $price . '</span>';
@@ -107,12 +111,10 @@ function my_theme_setup() {
 }
 add_action('after_setup_theme', 'my_theme_setup');
 
-
 function my_theme_add_editor_styles() {
     add_editor_style('editor-style.css');
 }
 add_action('admin_init', 'my_theme_add_editor_styles');
-
 
 function my_theme_custom_editor_buttons($buttons) {
     // اضافه کردن دکمه‌های سفارشی
@@ -127,7 +129,6 @@ function my_theme_custom_editor_buttons_2($buttons) {
     return $buttons;
 }
 add_filter('mce_buttons_2', 'my_theme_custom_editor_buttons_2');
-
 
 function custom_comment_template($comment, $args, $depth) {
     $GLOBALS['comment'] = $comment;
